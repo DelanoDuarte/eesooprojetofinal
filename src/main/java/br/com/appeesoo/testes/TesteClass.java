3c34f9d2f3be84079889bf980d4610327798e85c
Jéssica Paiva <Jssica Paiva>
2016-10-24 16:40:23 -0400

    Preparando a Aplicacao para a Web !

--- src/main/java/br/com/appeesoo/testes/TesteClass.java
+++ src/main/java/br/com/appeesoo/testes/TesteClass.java
@@ -3,10 +3,7 @@
  */
 package br.com.appeesoo.testes;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
-
-import br.com.appeesoo.domain.Cliente;
+import br.com.appeesoo.domain.Pedido;
 
 /**
  * @author Delano
@@ -17,20 +14,17 @@ public class TesteClass {
 	/**
 	 * @param args
 	 */
-
 	public static void main(String[] args) {
 
-		Cliente cliente = new Cliente();
-		cliente.setNome("Teste");
-		cliente.setCpf("342423423");
-		cliente.setCidade("Teste");
-		cliente.setEstado("Teste");
+		Pedido pedido = new Pedido();
 
-		EntityManager entityManager = Persistence.createEntityManagerFactory("ProjetoFinal-PU").createEntityManager();
-		entityManager.getTransaction().begin();
-		entityManager.persist(cliente);
-		entityManager.getTransaction().commit();
-		entityManager.close();
+		pedido.aceitarPedido(pedido);
+
+		System.out.println(pedido.getStatus().toString());
+
+		pedido.aceitarPagamentoPedido(pedido);
+
+		System.out.println(pedido.getStatus().toString());
 	}
 
 }
